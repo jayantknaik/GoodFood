c3db7f94fff060cfc895a14d1f925d693081ec04
Jayant Kumar Naik <jayant.naik@magicbricks.com>
2026-03-19 16:26:04 +0530

    Refactor API_URL configuration to use SERVER_URL from environment variables

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -11,7 +11,7 @@ const Cart = () => {
     const cartItems = useSelector(state => state.cart.items);
     const dispatch = useDispatch();
     const [subTotal, setSubTotal] = useState(0);
-    const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://goodfoodserver-tecq.onrender.com";
+    const API_URL = process.env.SERVER_URL;
 
     console.log("NODE_ENV: ", process.env.NODE_ENV);
     console.log("API_URL: ", API_URL);
